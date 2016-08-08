33866667f5551075184ccb7abfbdb97e079b9710
LeiLovell <leigh_lovell@hotmail.com>
2016-08-08 15:23:10 +0100

    Update default.py

--- default.py
+++ default.py
@@ -37,7 +37,7 @@ def INDEX():
     setView('movies', 'MAIN')
 
 def BUILDMENU():
-    addDir('UPDATE',BASEURL+'/s/3pqxx8mkz5b8wwo/0721.zip?dl=1',5,ART+'downloads.png',FANART,'')
+    addDir('UPDATE',BASEURL+'/s/syq9d3shupv0fj5/0808.zip?dl=1',5,ART+'downloads.png',FANART,'')
     setView('movies', 'MAIN')
     
 #def MAINTENANCE():
